df17f91f186b489820c13bc8c92db715fae7b711
rolmo <rodrigolmo.dev@gmail.com>
2025-08-12 04:28:21 -0300

    Addapt DefaultButton for mobile and accessibility

--- src/buttons/DefaultButton.vue
+++ src/buttons/DefaultButton.vue
@@ -2,28 +2,36 @@
 
 defineProps({
     label: String,
-    iconClasses: String
+    ariaLabel: {
+        type: String,
+        required: true
+    },
+    iconClasses: String,
+
 })
+
+const emit = defineEmits(['click'])
+const buttonClick = () => emit('click')
 
 </script>
 
 <template>
-    <button @click=registerUser class="
+    <button @click="buttonClick" :aria-label=ariaLabel class="
     bg-edark-gray 
-                text-elight px-4 py-2 
-                text-sm font-medium 
-                rounded-full 
-                hover:bg-edark 
-                cursor-pointer 
-                active:scale-99 
-                active:bg-edarker
-                capitalize
-                flex
-                gap-2
-                items-center
-                jutify-center">
+    text-elight px-4 py-2 
+    text-sm font-medium 
+    rounded-full 
+    hover:bg-edark 
+    cursor-pointer 
+    active:scale-99 
+    active:bg-edarker   
+    capitalize
+    flex
+    gap-2
+    items-center
+    jutify-center">
 
         <i :class="iconClasses"></i>
-        <span> {{ label }}</span>
+        <span class="hidden md:inline-block"> {{ label }}</span>
     </button>
 </template>
